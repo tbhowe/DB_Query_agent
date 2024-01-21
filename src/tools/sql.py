9c8b33dcb0227f99a4b568545ff4e38c080d1e70
Tim Howe <tbhowe@gmail.com>
2024-01-21 18:09:45 +0000

    Adds customised argument names via argument schemas

--- src/tools/sql.py
+++ src/tools/sql.py
@@ -1,13 +1,25 @@
 from langchain.tools import Tool
 from agent_tools import DatabaseConnector
+from pydantic.v1 import BaseModel
+from typing import List 
 
 connector = DatabaseConnector('prod_creds.yaml')
 print("connector created")
 
+class RunQueryArgsSchema(BaseModel):
+    """Schema for the arguments of the run_query_tool tool."""
+    query: str
+
+class ListColumnsArgsSchema(BaseModel):
+    """Schema for the arguments of the list_columns_tool tool."""
+    table_name: str
+    
+
 run_query_tool = Tool.from_function(
                                     name = "run_sql_query",
                                     description = "Run a postgresql SELECT query, returns the result of the query.",
-                                    func = connector.execute_query
+                                    func = connector.execute_query,
+                                    args_schema=RunQueryArgsSchema
                                     )
 
 list_tables_tool = Tool.from_function(
@@ -19,5 +31,7 @@ list_tables_tool = Tool.from_function(
 list_columns_tool = Tool.from_function(
                                     name = "list_columns",
                                     description = "Given a table name as an input, Returns the column names for that table.",
-                                    func = connector.get_table_columns
+                                    func = connector.get_table_columns,
+                                    args_schema=ListColumnsArgsSchema
                                     )
+                                    
