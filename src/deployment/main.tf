b9a24bbbee3054ee495e85eeaa955b6d8bb50827
Tim Howe <tbhowe@gmail.com>
2024-01-22 17:25:14 +0000

    Basic deployment code for TF-Azure container

--- src/deployment/main.tf
+++ src/deployment/main.tf
@@ -9,33 +9,97 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "example" {
   name     = "example-resources"
-  location = "West Europe"
+  location = var.location
 }
 
 resource "azurerm_container_group" "example" {
-  name                = "example-containergroup"
+  name                = "example-container-group"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   os_type             = "Linux"
+  ip_address_type     = "Public"
+  dns_name_label      = "example-container-group-dns"
 
   container {
-    name   = "example-container"
-    image  = "yourdockerhubusername/yourimage:yourtag"
-    cpu    = "0.5"
-    memory = "1.5"
+    name   = "gradio-app"
+    image  = "your_dockerhub_username/your_image_name:your_tag"
+    cpu    = 0.5
+    memory = 1.5
 
     ports {
       port     = 7860
       protocol = "TCP"
     }
-  }
 
-  ip_address {
-    type              = "Public"
-    dns_name_label    = "example-app"
-    ports {
-      protocol      = "TCP"
-      port          = 7860
+    environment_variables = {
+      "OpenAI_API_Key" = azurerm_key_vault_secret.openai_api_key.value
+      "RDS_HOST"       = azurerm_key_vault_secret.rds_host.value
+      "RDS_PASSWORD"   = azurerm_key_vault_secret.rds_password.value
+      "RDS_DATABASE"   = azurerm_key_vault_secret.rds_database.value
+      "RDS_PORT"       = azurerm_key_vault_secret.rds_port.value
     }
   }
+
+  identity {
+    type = "UserAssigned"
+    identity_ids = [azurerm_user_assigned_identity.example.id]
+  }
+}
+
+
+resource "azurerm_key_vault" "example" {
+  name                        = "example-key-vault"
+  location                    = azurerm_resource_group.example.location
+  resource_group_name         = azurerm_resource_group.example.name
+  tenant_id                   = var.tenant_id
+  sku_name                    = "standard"
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+}
+
+resource "azurerm_key_vault_secret" "openai_api_key" {
+  name         = "OpenAI-API-Key"
+  value        = var.openai_api_key
+  key_vault_id = azurerm_key_vault.example.id
+}
+
+resource "azurerm_key_vault_secret" "rds_host" {
+  name         = "RDS-HOST"
+  value        = var.rds_host
+  key_vault_id = azurerm_key_vault.example.id
+}
+
+resource "azurerm_key_vault_secret" "rds_password" {
+  name         = "RDS-PASSWORD"
+  value        = var.rds_password
+  key_vault_id = azurerm_key_vault.example.id
+}
+
+resource "azurerm_key_vault_secret" "rds_database" {
+  name         = "RDS-DATABASE"
+  value        = var.rds_database
+  key_vault_id = azurerm_key_vault.example.id
+}
+
+resource "azurerm_key_vault_secret" "rds_port" {
+  name         = "RDS-PORT"
+  value        = var.rds_port
+  key_vault_id = azurerm_key_vault.example.id
+}
+
+resource "azurerm_user_assigned_identity" "example" {
+  name                = "example-user-assigned-identity"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+}
+
+resource "azurerm_key_vault_access_policy" "example" {
+  key_vault_id = azurerm_key_vault.example.id
+
+  tenant_id = var.tenant_id
+  object_id = azurerm_user_assigned_identity.example.principal_id
+
+  key_permissions    = []
+  secret_permissions = ["get"]
+  certificate_permissions = []
 }
--- src/deployment/variables.tf
+++ src/deployment/variables.tf
@@ -18,3 +18,24 @@ variable "location" {
   description = "Location for all resources"
   default     = "UK South"
 }
+
+variable "openai_api_key" {
+  description = "OpenAI API key"
+}
+
+variable "rds_host" {
+  description = "RDS Host"
+}
+
+variable "rds_password" {
+  description = "RDS Password"
+}
+
+variable "rds_database" {
+  description = "RDS Database name"
+}
+
+variable "rds_port" {
+  description = "RDS Port"
+}
+
